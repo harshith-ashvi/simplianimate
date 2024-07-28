90414972600e6fc197240f85e8a356371c40126c
Harshith Ashvi <harshith.ashvi@gmail.com>
2024-07-28 08:27:00 +0530

    add terms and conditions page

---
+++ src/app/terms-and-condition/page.tsx
@@ -0,0 +1,149 @@
+export default function TermsAndCondition() {
+  return (
+    <div className="relative max-w-7xl mx-auto mt-20 pb-20">
+      <div className="flex flex-col justify-center items-center px-10 md:px-20">
+        <h1 className="text-4xl font-bold">Terms of Service</h1>
+
+        <div className="max-w-4xl mx-auto mt-8 text-gray-700 pb-16 border-2 border-black p-10 rounded-xl mb-10">
+          <div>
+            <p className="mt-2">1. Terms</p>
+            <p className="mt-2">
+              By accessing the website at https://simplianimate.com or
+              https://app.simplianimate.com, you are agreeing to be bound by
+              these terms of service, all applicable laws and regulations, and
+              agree that you are responsible for compliance with any applicable
+              local laws.
+              <br />
+              If you do not agree with any of these terms, you are prohibited
+              from using or accessing this site.
+              <br />
+              The materials contained in this website are protected by
+              applicable copyright and trademark law.
+            </p>
+
+            <p className="mt-4">2. Use License</p>
+            <p className="mt-2">
+              Permission is granted to temporarily download one copy of the
+              materials (information or software) on SimpliAnimate's website for
+              personal, non-commercial transitory viewing only. This is the
+              grant of a license, not a transfer of title, and under this
+              license you may not:
+              <br />
+              modify or copy the materials;
+              <br />
+              use the materials for any commercial purpose, or for any public
+              display (commercial or non-commercial);
+              <br />
+              attempt to decompile or reverse engineer any software contained on
+              SimpliAnimate's website;
+              <br />
+              remove any copyright or other proprietary notations from the
+              materials; or
+              <br />
+              transfer the materials to another person or "mirror" the materials
+              on any other server.
+              <br />
+              This license shall automatically terminate if you violate any of
+              these restrictions and may be terminated by SimpliAnimate at any
+              time. Upon terminating your viewing of these materials or upon the
+              termination of this license, you must destroy any downloaded
+              materials in your possession whether in electronic or printed
+              format.
+              <br />
+              SimpliAnimate may display your logo on it's website, app, or any
+              other marketing materials.
+            </p>
+
+            <p className="mt-4">3. Disclaimer</p>
+            <p className="mt-2">
+              The materials on SimpliAnimate's website are provided on an 'as
+              is' basis. SimpliAnimate makes no warranties, expressed or
+              implied, and hereby disclaims and negates all other warranties
+              including, without limitation, implied warranties or conditions of
+              merchantability, fitness for a particular purpose, or
+              non-infringement of intellectual property or other violation of
+              rights. Further, SimpliAnimate does not warrant or make any
+              representations concerning the accuracy, likely results, or
+              reliability of the use of the materials on its website or
+              otherwise relating to such materials or on any sites linked to
+              this site. You need to visit the privacy policy in tandem to the
+              terms.
+            </p>
+
+            <p className="mt-4">4. Limitations</p>
+            <p className="mt-2">
+              In no event shall SimpliAnimate or its suppliers be liable for any
+              damages (including, without limitation, damages for loss of data
+              or profit, or due to business interruption) arising out of the use
+              or inability to use the materials on SimpliAnimate's website, even
+              if SimpliAnimate or a SimpliAnimate authorized representative has
+              been notified orally or in writing of the possibility of such
+              damage. Because some jurisdictions do not allow limitations on
+              implied warranties, or limitations of liability for consequential
+              or incidental damages, these limitations may not apply to you.
+            </p>
+
+            <p className="mt-4">5. Accuracy of materials</p>
+
+            <p className="mt-2">
+              The materials appearing on SimpliAnimate's website could include
+              technical, typographical, or photographic errors. SimpliAnimate
+              does not warrant that any of the materials on its website are
+              accurate, complete or current. SimpliAnimate may make changes to
+              the materials contained on its website at any time without notice.
+              However SimpliAnimate does not make any commitment to update the
+              materials.
+            </p>
+
+            <p className="mt-4">6. Links</p>
+
+            <p className="mt-2">
+              SimpliAnimate has not reviewed all of the sites linked to its
+              website and is not responsible for the contents of any such linked
+              site. The inclusion of any link does not imply endorsement by
+              SimpliAnimate of the site. Use of any such linked website is at
+              the user's own risk.
+            </p>
+
+            <p className="mt-4">7. Cancellation &amp; Refunds</p>
+
+            <p className="mt-2">
+              You can cancel your subscription anytime (When paid plan starts).
+              <br />
+              In case of yearly plan, you will get a refund on pro-rata basis
+              excluding the month of cancellation and payment gateway charges.
+              <br />
+              In the case of monthly plans, you won't get any refund.
+            </p>
+
+            <p className="mt-4">8. Fair Usage Policy</p>
+            <p className="mt-2">
+              You may use SimpliAnimate for personal and commercial purposes
+              fairly. You shall not abuse the platform under any circumstance.
+              <br />
+              You may use SimpliAnimate for unlimited videos under fair usage
+              policy. The limits are too high to reach by anyone with a genuine
+              use case. In the case of constant overage you will be asked to
+              upgrade to a higher plan.
+            </p>
+
+            <p className="mt-4">9. Modifications</p>
+            <p className="mt-2">
+              SimpliAnimate may revise these terms of service for its website at
+              any time without notice. By using this website you are agreeing to
+              be bound by the then current version of these terms of service.
+            </p>
+
+            <p className="mt-4">10. Governing Law</p>
+            <p className="mt-2">
+              These terms and conditions are governed by and construed in
+              accordance with the laws of India and you irrevocably submit to
+              the exclusive jurisdiction of the courts in that State or
+              location.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
