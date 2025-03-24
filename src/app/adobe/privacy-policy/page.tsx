1e79fccb25882c26f892ad4bf11be01215653603
Harshith Ashvi <harshith.ashvi@gmail.com>
2025-03-24 08:58:18 +0530

    add privacy policy for adobe

---
+++ src/app/adobe/privacy-policy/page.tsx
@@ -0,0 +1,102 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="relative max-w-7xl mx-auto mt-20 pb-20">
+      <div className="flex flex-col justify-center items-center px-10 md:px-20">
+        <h1 className="text-4xl font-bold">Privacy Policy</h1>
+        <p className="text-xs mt-1">Effective date: 23/03/2025</p>
+
+        <div className="max-w-4xl mx-auto mt-8 text-gray-700 pb-16 border-2 border-black p-10 rounded-xl mb-10">
+          <div>
+            <p className="mt-2">1. Introduction</p>
+            <p className="mt-2">
+              Welcome to SimpliAnimate (“us”, “we”, or “our”) operates
+              SimpliAnimate (website/mobile application) (hereinafter referred
+              to as “Service”). Our Privacy Policy governs your visit to
+              SimpliAnimate Adobe Express Addons (website/mobile application),
+              and explains how we collect, safeguard and disclose information
+              that results from your use of our Service. We use your data to
+              provide and improve Service. By using Service, you agree to the
+              collection and use of information in accordance with this policy.
+              Unless otherwise defined in this Privacy Policy, the terms used in
+              this Privacy Policy have the same meanings as in our Terms and
+              Conditions. Our Terms and Conditions govern all use of our Service
+              and together with the Privacy Policy constitute your agreement
+              with us (“Agreement”).
+            </p>
+            <p className="mt-4">2. Information We May Collect</p>
+            <p className="mt-2">
+              Currently, we do not collect any personal information or usage
+              data from users of our addon. However, in the future, we may
+              collect the following types of information: <br />
+              <li>
+                <strong>User ID:</strong> A unique identifier associated with
+                your account or usage of our addon
+              </li>
+              <li>
+                <strong>Usage Data:</strong> Information on how frequently you
+                use our addon, such as the number of times it is used.
+              </li>
+            </p>
+            <p className="mt-4">3. How We May Use Your Information</p>
+            <p className="mt-2">
+              If we begin collecting data, it will be used for the following
+              purposes:
+              <li>To identify and track how users interact with our addon.</li>
+              <li>
+                To improve the features and performance of the addon based on
+                usage patterns.
+              </li>
+              <li>
+                To help us analyze usage and provide a better user experience.
+              </li>
+              We will not share this data with third parties, and we will not
+              use it for marketing or other unrelated purposes.
+            </p>
+            <p className="mt-4">4. Data Storage and Retention</p>
+            <p className="mt-2">
+              If we start collecting data, we will store it securely. We will
+              retain any collected data only for as long as necessary to improve
+              the addon’s functionality. Once the data is no longer needed, it
+              will be deleted.
+            </p>
+            <p className="mt-4">5. Third-Party Services</p>
+            <p className="mt-2">
+              Our addon does not currently rely on any third-party services that
+              collect personal data. If we begin to use any third-party services
+              in the future, we will update this privacy policy to provide you
+              with more information.
+            </p>
+            <p className="mt-4">6. Security</p>
+            <p className="mt-2">
+              We will implement appropriate measures to protect the data we
+              collect from unauthorized access, disclosure, alteration, or
+              destruction. However, please be aware that no data transmission
+              over the internet is completely secure, and we cannot guarantee
+              the absolute security of any data transmitted.
+            </p>
+            <p className="mt-4">7. Changes to This Privacy Policy</p>
+            <p className="mt-2">
+              We reserve the right to update or modify this Privacy Policy in
+              the future. If we begin collecting data, we will update this
+              policy to reflect those changes so we encourage you to review this
+              policy periodically.
+            </p>
+            <p className="mt-4">8. Your Rights</p>
+            <p className="mt-2">
+              You will have the right to request information about the data we
+              collect, request corrections to the data, or request deletion of
+              your data once we begin collecting it. If you wish to exercise
+              these rights, you can contact us using the contact information
+              provided below.
+            </p>
+            <p className="mt-4">9. Contact Us</p>
+            <p className="mt-2">
+              If you have any questions about this Privacy Policy, please
+              contact us: astroashvi@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
